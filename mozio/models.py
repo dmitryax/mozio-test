1079c78b7e4d2e6c2542cee661b325bbc9e9468f
Dmitry <anoshindx@gmail.com>
2014-05-03 19:26:04 +0800

    Change dimension of the geo cache table
    - Changed fields types (lat/lng) in Point model from Decimal to Int
    - Improved performance of saving data in the table

--- mozio/models.py
+++ mozio/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Didgits number for databese point(lat/lng) representation
-POINT_ACCURACY = 0
 
 class Company(models.Model):
     name = models.CharField(max_length=200)
@@ -21,6 +19,6 @@ class Vertex(models.Model):
 
 class Point(models.Model):
     """Point in cache square table representing companies' service area"""
-    lat = models.DecimalField(max_digits=POINT_ACCURACY+3, decimal_places=POINT_ACCURACY)
-    lng = models.DecimalField(max_digits=POINT_ACCURACY+3, decimal_places=POINT_ACCURACY)
+    lat = models.IntegerField()
+    lng = models.IntegerField()
     company = models.ForeignKey(Company)
--- mozio/utils.py
+++ mozio/utils.py
@@ -1,19 +1,28 @@
-from .models import POINT_ACCURACY, Vertex, Point
+from .models import Vertex, Point
+
+
+# Size(lng/lat) of cell for storing service aria data in cache table
+# Changing this will corrupt all the data in Point model
+CELL_SIZE = 0.2
+
 
 def save_polygon_in_table(company, polygon):
     vertices = Vertex.objects.filter(polygon=polygon)
     if not vertices:
         return
 
+    # Manipulating points(lat/lng) in integers for better perffomance
+    vertices = [[int(v.lat / CELL_SIZE), int(v.lng / CELL_SIZE)] for v in vertices]
+
     path_length = len(vertices)
-    start_point = vertices[0].lat, vertices[0].lng
+    start_point = vertices[0]
 
     def point_inside_polygon(x, y):
         inside = False
 
         p1x, p1y = start_point
         for i in range(path_length + 1):
-            p2x, p2y = vertices[i % path_length].lat, vertices[i % path_length].lng
+            p2x, p2y = vertices[i % path_length]
             if y > min(p1y, p2y):
                 if y <= max(p1y, p2y):
                     if x <= max(p1x, p2x):
@@ -25,19 +34,12 @@ def save_polygon_in_table(company, polygon):
 
         return inside
 
-    min_lat = min(v.lat for v in vertices)
-    max_lat = max(v.lat for v in vertices)
-    min_lng = min(v.lng for v in vertices)
-    max_lng = max(v.lng for v in vertices)
-    step = 1. / 10**POINT_ACCURACY
+    min_lat = min(v[0] for v in vertices)
+    max_lat = max(v[0] for v in vertices)
+    min_lng = min(v[1] for v in vertices)
+    max_lng = max(v[1] for v in vertices)
 
-    for lng in frange(min_lng, max_lng, step):
-        for lat in frange(min_lat, max_lat, step):
-            if point_inside_polygon(lat + step/2, lng + step/2):
+    for lng in xrange(min_lng, max_lng):
+        for lat in xrange(min_lat, max_lat):
+            if point_inside_polygon(lat, lng):
                 Point.objects.get_or_create(lat=lat, lng=lng, company=company)
-
-def frange(start, stop, step):
-    r = start
-    while r < stop:
-        yield r
-        r += step
